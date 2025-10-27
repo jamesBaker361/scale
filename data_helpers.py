34eca23409043045242df28ba0268f63e184f087
James Baker <jamesbaker@Jamess-MacBook-Air.local>
2025-10-27 11:07:53 -0400

    h

---
+++ data_helpers.py
@@ -0,0 +1,29 @@
+from torch.utils.data import Dataset
+from datasets import load_dataset
+from diffusers.image_processor import VaeImageProcessor
+from transformers import CLIPTokenizer
+
+class AnimalData(Dataset):
+    def __init__(self,image_processor:VaeImageProcessor,
+                 tokenizer:CLIPTokenizer,
+                 dim:int=128,
+                 hf_path:str="Rapidata/Animals-10"):
+        super().__init__()
+        self.data=load_dataset(hf_path,split="train")
+        self.image_processor=image_processor
+        self.tokenizer=tokenizer
+        self.dim=dim
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, index):
+        image=self.image_processor.preprocess(self.data["image"][index],self.dim,self.dim)[0]
+        text=self.tokenizer(
+            self.data["label"][index], max_length=self.tokenizer.model_max_length, padding="max_length", truncation=True, return_tensors="pt"
+        )
+        return {
+            "image":image,
+            "text":text
+        }
+        
