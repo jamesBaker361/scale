82d77dded6c81c509756c6d4e26123825ad57eb0
James Baker <jamesbaker@Jamess-MacBook-Air-4.local>
2025-10-30 19:14:36 -0400

    input ids

--- main.py
+++ main.py
@@ -178,7 +178,7 @@ def main(args):
             with accelerator.accumulate(params):
                 
                 images=batch["image"].to(device,dtype=torch_dtype)
-                text=batch["text"].to(device,) #dtype=torch_dtype)
+                text=batch["text"]['input_ids'].to(device,) #dtype=torch_dtype)
                 print(type(text))
                 print(text)
                 encoder_hidden_states = text_encoder(text, return_dict=False)[0]
