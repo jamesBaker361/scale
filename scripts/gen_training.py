56bd5e9084b74a477fdfc279b17658ce69f83b72
James Baker <jamesbaker@Jamess-MacBook-Air-4.local>
2025-11-07 15:18:04 -0500

    kjsdfa

--- scripts/gen_training.py
+++ scripts/gen_training.py
@@ -1,4 +1,6 @@
 for training_type in ["noise","scale","scale_vae"]:
-    command=f"sbatch --err=slurm_chip/trainingv1/{training_type}.err --out=slurm_chip/trainingv1/{training_type}.out "
-    command+=f" runpygpu_chip.sh main.py --save_dir {training_type} --name jlbaker361/{training_type} "
+    command=f"sbatch -J scale --err=slurm_chip/trainingv1/{training_type}.err --out=slurm_chip/trainingv1/{training_type}.out "
+    command+=f" --constraint=L40s runpygpu_chip_l40.sh "
+    command+=f" runpygpu_chip.sh "
+    command+=f"main.py --save_dir {training_type} --name jlbaker361/{training_type} "
     print(command)
