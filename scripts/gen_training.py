b49617adb9e5f2e3874fc72679e9ced82de839e0
James Baker <jamesbaker@ofrkbrfslt31.rad.rutgers.edu>
2025-11-06 23:48:20 -0500

    scripts

--- scripts/gen_training.py
+++ scripts/gen_training.py
@@ -1,3 +1,4 @@
 for training_type in ["noise","scale","scale_vae"]:
     command=f"sbatch --err=slurm_chip/trainingv1/{training_type}.err --out=slurm_chip/trainingv1/{training_type}.out "
     command+=f" runpypgpu_chip.sh main.py --save_dir {training_type} --name jlbaker361/{training_type} "
+    print(command)
