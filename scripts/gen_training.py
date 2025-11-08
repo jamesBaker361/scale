1384b9f83e18c4244331dd15355f9a1eef269b94
James Baker <jamesbaker@Jamess-MacBook-Air-4.local>
2025-11-07 19:33:15 -0500

    big steps

--- scripts/gen_training.py
+++ scripts/gen_training.py
@@ -2,5 +2,5 @@ for training_type in ["noise","scale","scale_vae"]:
     command=f"sbatch -J scale --err=slurm_chip/trainingv1/{training_type}.err --out=slurm_chip/trainingv1/{training_type}.out "
     command+=f" --constraint=L40s runpygpu_chip_l40.sh "
     #command+=f" runpygpu_chip.sh "
-    command+=f"main.py --save_dir {training_type} --repo_id jlbaker361/{training_type} --batch_size 1 --val_interval 1 --training_type {training_type} "
+    command+=f"main.py --save_dir {training_type} --repo_id jlbaker361/{training_type} --batch_size 4 --val_interval 10 --training_type {training_type} --limit -1 --epochs 100 --gradient_accumulation_steps 8 "
     print(command)
