be259ba4b77596c56c7939dee0deb5a05d519007
James Baker <jamesbaker@Jamess-MacBook-Air-9.local>
2025-11-15 21:54:12 -0500

    100 epochs

--- scripts/gen_training_data.py
+++ scripts/gen_training_data.py
@@ -3,6 +3,6 @@ for training_type in ["noise","scale","scale_vae"]:
     command=f"sbatch -J scale --err=slurm_chip/trainingv1/{training_type}_catdog.err --out=slurm_chip/trainingv1/{training_type}_catdog.out "
     command+=f" --constraint=L40s runpygpu_chip_l40.sh "
     #command+=f" runpygpu_chip.sh "
-    command+=f"main.py --save_dir {training_type}_catdog --repo_id jlbaker361/{training_type}_catdog --batch_size 4 --val_interval 1 --training_type {training_type} --limit 10 --epochs 2 --gradient_accumulation_steps 8 "
+    command+=f"main.py --save_dir {training_type}_catdog --repo_id jlbaker361/{training_type}_catdog --batch_size 4 --val_interval 10 --training_type {training_type} --limit -1 --epochs 100 --gradient_accumulation_steps 8 "
     command+=f" --hf_path_data microsoft/cats_vs_dogs "
     print(command)
